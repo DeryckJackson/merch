335ff77e45f436beada4b773bda3da30508f556b
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-09-22 09:04:46 -0700

    commit for gh-pages

---
+++ precache-manifest.fd93ceec28f4dc17f087e859cf95f1de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83abe3d65a8b21b4213098ab02d1bf88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d2bb963c14811d8e810",
+    "url": "/static/css/main.b2d21a7d.chunk.css"
+  },
+  {
+    "revision": "7d89f236e1e7b820b9bf",
+    "url": "/static/js/2.0298ea48.chunk.js"
+  },
+  {
+    "revision": "1d2bb963c14811d8e810",
+    "url": "/static/js/main.9c0242ce.chunk.js"
+  },
+  {
+    "revision": "49746c565457095e0b53",
+    "url": "/static/js/runtime-main.9f025be2.js"
+  }
+]);
--- src/reducers/cart_visible_reducer.js
+++
@@ -1,8 +0,0 @@
-export default (state = false, action) => {
-  switch (action.type) {
-  case 'TOGGLE_CART':
-    return !state;
-  default:
-    return state;
-  }
-};
